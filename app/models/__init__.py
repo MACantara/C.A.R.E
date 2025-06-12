5d5777305a4fef599057e7bd8c266fbe162e50c3
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-06-12 14:14:48 +0800

    Add Appointment and AppointmentReminder to model exports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,14 @@ from .user import User, PasswordResetToken
 from .contact import Contact
 from .login_attempt import LoginAttempt
 from .email_verification import EmailVerification
+from .appointment import Appointment, AppointmentReminder
 
-__all__ = ['User', 'PasswordResetToken', 'Contact', 'LoginAttempt', 'EmailVerification']
+__all__ = [
+    "User",
+    "PasswordResetToken",
+    "Contact",
+    "LoginAttempt",
+    "EmailVerification",
+    "Appointment",
+    "AppointmentReminder",
+]
